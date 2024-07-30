2bf4c584ea575d3b5c840bdea19f1458e1dc3a27
vikrant-1910 <144235308+vikrant-1910@users.noreply.github.com>
2024-07-30 12:44:57 +0530

    Update musiclibrary.py

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,5 +1,3 @@
 music ={
-    "songs" : "https://www.youtube.com/shorts/eaiPIFP1V70",
-    "skyfall" : "https://www.youtube.com/watch?v=DeumyOzKqgI",
-    "nacho" : "https://www.youtube.com/watch?v=sAzlWScHTc4",   
+    "songs" : "song link"   
 }

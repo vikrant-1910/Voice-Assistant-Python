1f2f78b8136eb78a7e3f8b2c533b9696fd1fdfda
vikrant-1910 <144235308+vikrant-1910@users.noreply.github.com>
2024-07-30 12:43:42 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,5 @@
+music ={
+    "songs" : "https://www.youtube.com/shorts/eaiPIFP1V70",
+    "skyfall" : "https://www.youtube.com/watch?v=DeumyOzKqgI",
+    "nacho" : "https://www.youtube.com/watch?v=sAzlWScHTc4",   
+}
